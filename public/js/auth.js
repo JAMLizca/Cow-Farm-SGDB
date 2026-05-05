c47a48e708dcd876e827bd3b36679c70228d99b5
Alejandro Montenegro. <josemontenegroqc@gmail.com>
2026-05-05 12:10:55 -0500

    Update API_URL auth.js

--- public/js/auth.js
+++ public/js/auth.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://127.0.0.1:8000/api';
+const API_URL = 'https://tu-app-production.up.railway.app/api';
 
 document.getElementById('btn-login').addEventListener('click', async () => {
 
